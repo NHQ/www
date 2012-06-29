1cca5bd587221d94a6184baecaec2554e399c778
Angeline Gragasin <angelinegragasin@tarkovsky.att.net>
2012-06-29 13:39:09 -0700

    almost rude

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ server.use(function(req, res, next){
 });
 server.use(connect.cookieParser('keyboard cat'))
 			.use(connect.bodyParser())
-			.use(connect.session({ secret: 'whatwhat', key: 'sid', cookie: { secure: true }}))
 			.use(function(req, res, next){
 				req.domani = req.headers.host.split('.');
 				johnny(req, res);
@@ -23,7 +22,7 @@ server.use(connect.cookieParser('keyboard cat'))
 			});
 //			.use(auth)
 			
-server.listen(3009);
+server.listen(3009, '127.0.0.1');
 
 function Static (req, res){
 	
